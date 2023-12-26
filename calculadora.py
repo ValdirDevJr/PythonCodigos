2d27aedf87571467613636917b3cadb6477e1acb
Valdir Pereira <140351726+ValdirDevJr@users.noreply.github.com>
2023-12-26 15:17:17 -0300

    calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,4 @@
-# ~~~~~~~~~~~~~~~~~~ DESAFIO PRÁTICO ~~~~~~~~~~~~~~~~~~
 #                      Calculadora
-
-# Crie uma calculadora com opções de soma, multiplicação, subtração, divisão e sair.
-
-# (Ela deverá funcionar infinitamente, até que o usuário decida sair da calculadora.)
-
-# Utilize funções de rotina para cada operação e funções de unidade lógica para realizar os cálculos.
-
-# Dica:
-# Utilize de condicionais e Laços de Repetição para realizar esse exercício.
 
 
 def somar(a, b):
@@ -24,41 +14,56 @@ def subtração(a, b):
 
 
 def divisão(a, b):
-    return a / b
+    if b != 0:
+        return a / b
+    else:
+        print('Erro: Divisão por zero')
+        return None
 
 
 while True:
-    print("Escolha uma operação:")
-    print("[1] - Soma")
-    print("[2] - Subtração")
-    print("[3] - Multiplicação")
-    print("[4] - Divisão")
-    print("[5] - Sair")
+    print(
+        '''Escolha uma operação
+          
+[1] - Soma
+[2] - Subtração
+[3] - Multiplicação
+[4] - Divisão
+[5] - Sair
 
-    escolha = input("Escolha a operação desejada: ")
+'''
+    )
 
-    if escolha == "5":
-        print("Calculadora encerrada")
+    escolha = input('Escolha a operação desejada: ')
+
+    if escolha == '5':
+        print('Calculadora encerrada')
         break
 
-    num1 = int(input("Digite o primeiro número: "))
-    num2 = int(input("Digite o segundo número: "))
+    if escolha not in ['1', '2', '3', '4']:
+        print('Opção inválida. Tente novamente.')
+        continue  # Volta ao início do loop sem solicitar num1 e num2
 
-    if escolha == "1":
-        resultado = somar(num1, num2)
-        print(f"{num1} + {num2} = {resultado}")
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
 
-    if escolha == "2":
-        resultado = subtração(num1, num2)
-        print(f"{num1} - {num2} = {resultado}")
+    match escolha:
+        case '1':
+            resultado = somar(num1, num2)
+            print(f'{num1} + {num2} = {resultado}')
 
-    if escolha == "3":
-        resultado = somar(num1, num2)
-        print(f"{num1} * {num2} = {resultado}")
+        case '2':
+            resultado = subtração(num1, num2)
+            print(f'{num1} - {num2} = {resultado}')
 
-    if escolha == "4":
-        resultado = subtração(num1, num2)
-        print(f"{num1} / {num2} = {resultado}")
+        case '3':
+            resultado = somar(num1, num2)
+            print(f'{num1} * {num2} = {resultado}')
 
-    else:
-        ("Comando inválido. Tente novamente")
+        case '4':
+            resultado = subtração(num1, num2)
+            if resultado is not None:
+                print(f'{num1} / {num2} = {resultado}')
+
+        case _:
+            ('Comando inválido. Tente novamente')
